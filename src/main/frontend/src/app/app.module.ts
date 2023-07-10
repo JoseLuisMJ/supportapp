156a031ab17fbac3d922e9b571f3ed2c6e207839
JoseLuisMJ <trepa01@hotmail.com>
2023-07-10 09:45:11 +0200

    created components

--- src/main/frontend/src/app/app.module.ts
+++ src/main/frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListComponent } from './list/list.component';
+import { CreateformComponent } from './createform/createform.component';
+import { EditformComponent } from './editform/editform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListComponent,
+    CreateformComponent,
+    EditformComponent
   ],
   imports: [
     BrowserModule,
